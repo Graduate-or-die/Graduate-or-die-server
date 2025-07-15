7015ffe377fe8ce32a3383471ac1d2011709f0a2
hyeonseo <gustj7038@gmail.com>
2025-07-15 15:22:02 +0900

    update: 초기 세팅

---
+++ pome-server/pome-server/src/test/java/com/pome/PomeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PomeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
