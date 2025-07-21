9c816567ea513bd2e3479afb8f4c8938e57ff68c
hyeonseo <gustj7038@gmail.com>
2025-07-22 00:06:14 +0900

    feat: 유저 도메인 구현

---
+++ pome-server/src/main/java/server/pome/global/domain/User.java
@@ -0,0 +1,60 @@
+package server.pome.global.domain;
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "users")
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(nullable = false, unique = true)
+  private Long id;
+
+  @ElementCollection
+  @CollectionTable(name = "user_from_ids", joinColumns = @JoinColumn(name = "user_id"))
+  @Column(name = "from_id")
+  @Comment("신청자 아이디 목록")
+  private List<Integer> fromIds;
+
+  @Column(name = "user_name", nullable = false, unique = true)
+  @Comment("유저 닉네임")
+  private String userName;
+
+  @Column(name = "password", nullable = false)
+  @Comment("비밀번호")
+  private String password;
+
+  @Column(name = "matching", nullable = false, columnDefinition = "TINYINT(1) DEFAULT 1")
+  @Comment("매칭 활성화 여부 / 기본값 = True")
+  private Boolean matching;
+
+  @Column(name = "introduction", nullable = true, columnDefinition = "LONGTEXT")
+  @Comment("자기소개")
+  private String introduction;
+
+  @Column(name = "job", nullable = true, length = 20)
+  @Comment("희망 직무")
+  private String job;
+
+  @Column(name = "profile_image", length = 2048)
+  @Comment("프로필 사진")
+  private String profileImage;
+}
