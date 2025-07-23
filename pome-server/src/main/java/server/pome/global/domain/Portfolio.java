d6bffce87461e58ba4176c6180498ae1c95822f7
Haerim Lee <lhaerim1229@naver.com>
2025-07-24 01:27:17 +0900

    feat: 포트폴리오 도메인 구현

---
+++ pome-server/src/main/java/server/pome/global/domain/Portfolio.java
@@ -0,0 +1,42 @@
+package server.pome.global.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name="portfolios")
+public class Portfolio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, unique = true)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ElementCollection
+    @CollectionTable(name = "portfolio_tags", joinColumns = @JoinColumn(name = "portfolio_id"))
+    @Column(name = "tag")
+    @Comment("AI 태그 목록")
+    private List<String> tag = new ArrayList<>();
+
+    @ElementCollection(fetch = FetchType.LAZY)
+    @CollectionTable(name = "portfolio_visibility", joinColumns = @JoinColumn(name = "portfolio_id"))
+    @Column(name = "visibility_map")
+    @Comment("알람 여부 매핑")
+    private Map<String, Boolean> visibilityMap = new HashMap<>();
+}
