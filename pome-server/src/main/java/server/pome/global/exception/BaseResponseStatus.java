a3858ffc07013517831d578b0f10160e7c5e10ae
Haerim Lee <lhaerim1229@naver.com>
2025-08-17 03:17:41 +0900

    fix: 충돌해결

--- pome-server/src/main/java/server/pome/global/exception/BaseResponseStatus.java
+++ pome-server/src/main/java/server/pome/global/exception/BaseResponseStatus.java
@@ -43,8 +43,7 @@ public enum BaseResponseStatus {
   NOT_MATCHED_MATE(false, HttpStatus.BAD_REQUEST, 2014, "매칭된 메이트가 아닙니다."),
   CONFLICT_STATE(false, HttpStatus.BAD_REQUEST, 2015, "상태가 불일치하여 해제 실패하였습니다."),
   MATCHING_DISABLED(false, HttpStatus.BAD_REQUEST, 2016, "매칭 비활성화 상태입니다."),
-
-
+  
   // 3000번대: 응답 오류
   RESPONSE_ERROR(false, HttpStatus.BAD_REQUEST, 3000, "값을 불러오는데 실패하였습니다."),
 
