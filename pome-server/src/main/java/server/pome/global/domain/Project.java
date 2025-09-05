cc1efc76c8a2ff871375a9096659e1879cefc584
Haerim Lee <lhaerim1229@naver.com>
2025-09-05 15:41:46 +0900

    feat: 도메인 설정 #41

---
+++ pome-server/src/main/java/server/pome/global/domain/Project.java
@@ -0,0 +1,45 @@
+package server.pome.global.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.Comment;
+
+import java.time.LocalDate;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Entity
+@Table(name = "projects")
+public class Project extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "portfolio_id", nullable = false)
+    private Portfolio portfolio;
+
+    @Column(name = "project_name", nullable = true)
+    @Comment("프로젝트명")
+    private String projectName;
+
+    @Column(name = "project_start_at", nullable = true)
+    @Comment("시작일")
+    private LocalDate projectStartAt;
+
+    @Column(name = "project_end_at", nullable = true)
+    @Comment("마감일")
+    private LocalDate projectEndtAt;
+
+    @Column(name = "project_role", nullable = true)
+    @Comment("사용 기술 스택 및 역할")
+    private String projectRole;
+
+    @Column(name = "project_description", nullable = true)
+    @Comment("프로젝트 설명")
+    private String projectDescription;
+
+    @Column(name = "project_award", nullable = true)
+    @Comment("프로젝트 성과")
+    private String projectAward;
+
+}
