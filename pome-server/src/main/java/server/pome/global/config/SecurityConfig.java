d51703b9cd15b58c9d6ebbd034f353d809fcbf5d
hyeonseo <gustj7038@gmail.com>
2025-11-03 15:36:09 +0900

    fix: health check를 security 대상에서 제외

--- pome-server/src/main/java/server/pome/global/config/SecurityConfig.java
+++ pome-server/src/main/java/server/pome/global/config/SecurityConfig.java
@@ -2,27 +2,24 @@ package server.pome.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
-  @Bean @Order(0)
-  SecurityFilterChain actuatorChain(HttpSecurity http) throws Exception {
-    http.securityMatcher("/actuator/**")
-        .authorizeHttpRequests(a -> a.anyRequest().permitAll())
-        .csrf(c -> c.disable());
-    return http.build();
-  }
-
-  // 일반 API 체인
-  @Bean @Order(1)
-  SecurityFilterChain apiChain(HttpSecurity http) throws Exception {
-    http.csrf(c -> c.disable())
-        .authorizeHttpRequests(a -> a
-            .requestMatchers("/swagger-ui/**","/v3/api-docs/**").permitAll()
+  @Bean
+  SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http
+        .csrf(csrf -> csrf.ignoringRequestMatchers(
+            "/health/**", "health/check", "/actuator/**"
+        ))
+        .authorizeHttpRequests(auth -> auth
+            // GET health check은 항상 통과하도록(security 제외)
+            .requestMatchers(HttpMethod.GET,
+                "/health/**", "/actuator/health", "/actuator/health/**")
+            .permitAll()
             .anyRequest().authenticated()
         );
     return http.build();
