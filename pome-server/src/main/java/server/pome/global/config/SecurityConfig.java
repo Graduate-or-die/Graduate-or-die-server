13ee8588468c879ac2813c52ef83b9f20ccab4d8
hyeonseo <128580251+gustj3104@users.noreply.github.com>
2025-11-14 20:49:27 +0900

    [Feature] S3 연동, 파일 업로드, 삭제 API 구현 (#66)
    * feat: s3 설정
    
    * feat: cors 설정, csrf 해제

--- pome-server/src/main/java/server/pome/global/config/SecurityConfig.java
+++ pome-server/src/main/java/server/pome/global/config/SecurityConfig.java
@@ -2,26 +2,20 @@ package server.pome.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-
   @Bean
-  SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http
-        .csrf(csrf -> csrf.ignoringRequestMatchers(
-            "/health/**", "health/check", "/actuator/**"
-        ))
-        .authorizeHttpRequests(auth -> auth
-            // GET health check은 항상 통과하도록(security 제외)
-            .requestMatchers(HttpMethod.GET,
-                "/health/**", "/actuator/health", "/actuator/health/**")
-            .permitAll()
-            .anyRequest().authenticated()
-        );
+  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.sessionManagement(
+        (session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+    http.csrf(AbstractHttpConfigurer::disable);
+
     return http.build();
   }
 }
+
