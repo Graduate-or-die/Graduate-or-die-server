fbeb1fed3c0efb1db3b98d43039a435d0231412d
hyeonseo <128580251+gustj3104@users.noreply.github.com>
2026-02-27 18:51:11 +0900

    [Feature] 개발용 API 추가 및 프론트 콜백 url로 code 전송 추가 (#104)
    * feat: 테스트용 APi 구현
    
    * feat: 콜백 Url로 code 전송 구현

--- pome-server/src/main/java/server/pome/global/config/SecurityConfig.java
+++ pome-server/src/main/java/server/pome/global/config/SecurityConfig.java
@@ -28,6 +28,7 @@ public class SecurityConfig {
             .requestMatchers(
                     "/auth/login",
                     "/auth/login/**",
+                    "/auth/dev/**",
                     "/auth/reissue",
                     "/auth/logout",
                     "/swagger-ui/**",
